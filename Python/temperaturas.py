622bcc651b6606ea2a327f08736ba1c0dc606981
Veltys <veltys@veltys.es>
2017-07-04 03:30:28 +0200

    Commit para sincronización entre equipos

--- Python/temperaturas.py
+++ Python/temperaturas.py
@@ -12,9 +12,6 @@
 #                 Mandándole la señal "SIGUSR2", el sistema pasa a "modo apagado", lo cual simplemente apaga todos los leds hasta que esta misma señal sea recibida de nuevo
 
 
-modo_apagado = False
-
-
 import errno                                                                    # Códigos de error
 import sys                                                                      # Funcionalidades varias del sistema
 
@@ -28,93 +25,44 @@ except ImportError:
 from time import sleep	                                                        # Para hacer pausas
 from shlex import split				        	                                # Manejo de cadenas
 from subprocess import check_output		        	                            # Llamadas a programas externos, recuperando su respuesta
+import comun                                                                    # Funciones comunes a varios sistemas
 import os									                                    # Funcionalidades varias del sistema operativo
 import pid                                                                      # Módulo propio de acceso a las funciones relativas al PID
 import RPi.GPIO as GPIO                                                         # Acceso a los pines GPIO
-import signal		        				                                    # Manejo de señales
+
+class temperaturas(comun.app):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def bucle():
+        try:
+            while True:
+                for gpio in config.GPIOS:
+                    GPIO.output(gpio, GPIO.LOW)
+
+                if not(modo_apagado):
+                    temperatura = check_output(split('/opt/vc/bin/vcgencmd measure_temp'))
+                    temperatura = float(temperatura[5:-3])
+
+                    if temperatura < config.TEMPERATURAS[0]:
+                        GPIO.output(config.GPIOS[0], GPIO.HIGH)
+                    elif temperatura < config.TEMPERATURAS[1]:
+                        GPIO.output(config.GPIOS[1], GPIO.HIGH)
+                    elif temperatura < config.TEMPERATURAS[2]:
+                        GPIO.output(config.GPIOS[2], GPIO.HIGH)
+                    else:
+                        GPIO.output(config.GPIOS[3], GPIO.HIGH)
 
 
-def apagado():
-    global modo_apagado
+                sleep(config.PAUSA)
 
-    modo_apagado = not(modo_apagado)
-
-    for gpio in GPIOS:
-        GPIO.output(gpio, GPIO.LOW)
-
-
-def cerrar():
-    GPIO.cleanup()                                                              # Devolvemos los pines a su estado inicial
-    pid.desbloquear(os.path.basename(sys.argv[0]))
-    sys.exit()
-
-
-def test():
-    for gpio in config.GPIOS:
-        GPIO.output(gpio, GPIO.HIGH)
-
-
-def sig_apagado(signum, frame):
-    apagado()
-
-
-def sig_cerrar(signum, frame):
-    cerrar()
-
-
-def sig_test(signum, frame):
-    test()
-    sleep(config.PAUSA)
-
-
-def bucle():
-    try:
-        while True:
-            for gpio in config.GPIOS:
-                GPIO.output(gpio, GPIO.LOW)
-
-            if not(modo_apagado):
-                temperatura = check_output(split('/opt/vc/bin/vcgencmd measure_temp'))
-                temperatura = float(temperatura[5:-3])
-
-                if temperatura < config.TEMPERATURAS[0]:
-                    GPIO.output(config.GPIOS[0], GPIO.HIGH)
-                elif temperatura < config.TEMPERATURAS[1]:
-                    GPIO.output(config.GPIOS[1], GPIO.HIGH)
-                elif temperatura < config.TEMPERATURAS[2]:
-                    GPIO.output(config.GPIOS[2], GPIO.HIGH)
-                else:
-                    GPIO.output(config.GPIOS[3], GPIO.HIGH)
-
-
-            sleep(config.PAUSA)
-
-    except KeyboardInterrupt:
-        cerrar()
+        except KeyboardInterrupt:
+            self.cerrar()
 
 
 def main(argv = sys.argv):
-    signal.signal(signal.SIGTERM, sig_cerrar)
-    signal.signal(signal.SIGUSR1, sig_test)
-    signal.signal(signal.SIGUSR2, sig_apagado)
-
-    if pid.comprobar(os.path.basename(argv[0])):
-        if pid.bloquear(os.path.basename(argv[0])):
-            GPIO.setmode(GPIO.BCM)				                                        # Establecemos el sistema de numeración BCM
-            GPIO.setwarnings(False)				                                        # De esta forma no alertará de los problemas
-
-            for gpio in config.GPIOS:
-                GPIO.setup(gpio, GPIO.OUT)                                              # Configuramos los pines GPIO como salida
-
-            bucle()
-
-        else:
-            print('Error: No se puede bloquear ' + os.path.basename(argv[0]), file=sys.stderr)
-            sys.exit(errno.EACCES)
-
-    else:
-        print('Error: Ya se ha iniciado una instancia de ' + os.path.basename(argv[0]), file=sys.stderr)
-        sys.exit(errno.EEXIST)
+    app = temperaturas(config)
+    app.arranque(os.path.basename(argv[0]))
 
 
 if __name__ == '__main__':
