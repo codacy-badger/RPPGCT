2887702722a7b31eb5c18685f5031244f3bd9f2a
Veltys <veltys@veltys.es>
2017-11-30 00:32:25 +0100

    Fix: acceso incorrecto al método "GPIO"

--- Python/reiniciar_router.py
+++ Python/reiniciar_router.py
@@ -44,7 +44,7 @@ class reiniciar_router(comun.app):
     def bucle(self):
         try:
             if self._conectar('conectar ' + self._config.servidor, False):
-                self._enviar_y_recibir('apagar ' + GPIO[0])
+                self._enviar_y_recibir('apagar ' + _config.GPIO[0][0])
 
                 desconectar()
 
@@ -53,7 +53,7 @@ class reiniciar_router(comun.app):
             while True:
                 if hay_internet():                                                          # Si hay Internet, simplemente se esperará para hacer la próxima comprobación
                     if self._conectar('conectar ' + self._config.servidor, False):
-                        self._enviar_y_recibir('apagar ' + GPIO[0])
+                        self._enviar_y_recibir('apagar ' + _config.GPIO[0][0])
 
                         desconectar()
 
@@ -61,11 +61,11 @@ class reiniciar_router(comun.app):
 
                 else:                                                                       # En caso contrario, se mandará la orden de apagado durante el tiempo mínimo establecido y después se restablecerá
                     if self._conectar('conectar ' + self._config.servidor, False):
-                        self._enviar_y_recibir('encender ' + GPIO[0])
+                        self._enviar_y_recibir('encender ' + _config.GPIO[0][0])
 
                         sleep(self._config.PAUSA)
 
-                        self._enviar_y_recibir('apagar ' + GPIO[0])
+                        self._enviar_y_recibir('apagar ' + _config.GPIO[0][0])
 
                         desconectar()
 
