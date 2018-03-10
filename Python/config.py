1608566aac87de6ac1aac9a4497a177d85abfc0b
Unknown <veltys@veltys.es>
2018-03-10 22:23:29 +0100

    FIX: Fecha de modificación y versión

--- Python/config.py
+++ Python/config.py
@@ -5,8 +5,8 @@
 # Title         : config.py
 # Description   : Módulo configurador para ser importado en el resto de módulos o sistemas que lo necesiten
 # Author        : Veltys
-# Date          : 02-12-2017
-# Version       : 1.5.0
+# Date          : 07-03-2018
+# Version       : 1.6.0
 # Usage         : import config | from config import <clase>
 # Notes         : A título ilustrativo, a se ofrece una configuración por defecto (la mía, para ser exactos)
 
