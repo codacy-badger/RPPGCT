d59f4e4294052d82fd9cdfd25ad3fa78917c874b
Unknown <veltys@veltys.es>
2018-05-24 17:16:56 +0200

    FIX: Self

--- Python/config.py
+++ Python/config.py
@@ -11,8 +11,8 @@
 # Notes         : A título ilustrativo, a se ofrece una configuración por defecto (la mía, para ser exactos)
 
 
-class config_global:                                                                # Configuración común
-    IP_DEP_REMOTA   = '192.168.0.4'                                                 # IP del servidor de depuración
+class config_global:                                                                        # Configuración común
+    IP_DEP_REMOTA   = '192.168.0.4'                                                         # IP del servidor de depuración
 
     RELE            = 0
     LED             = 0
@@ -20,17 +20,17 @@ class config_global:                                                            
     SONDA           = 2
 
 
-''' class cpu_config(config_global):                                                # Configuración del sistema de CPU
-    GPIOS           = [(26, True,  True , self.LED  , 'Verde'                   ),  # GPIOS contiene ternas de datos en formato lista:
-                       (19, True,  True , self.LED  , 'Amarillo'                ),  # el primer elemento será el número (BCM) de puerto GPIO a manipular,
-                       (13, True,  True , self.LED  , 'Naranja'                 ),  # el segundo, el modo (True para salida, False para entrada)
-                       ( 6, True,  True , self.LED  , 'Rojo'                    ),  # el tercero, la activación si es de salida (True si es activo a alto nivel o False si es a bajo nivel) o el estado si es de entrada (True si está bajado y False subido)
-                       ( 5, True,  True , self.LED  , 'Alarma'                  ),  # y el cuarto, una muy breve descripción de su función
+''' class cpu_config(config_global):                                                        # Configuración del sistema de CPU
+    GPIOS           = [(26, True,  True , config_global.LED  , 'Verde'                   ), # GPIOS contiene ternas de datos en formato lista:
+                       (19, True,  True , config_global.LED  , 'Amarillo'                ), # el primer elemento será el número (BCM) de puerto GPIO a manipular,
+                       (13, True,  True , config_global.LED  , 'Naranja'                 ), # el segundo, el modo (True para salida, False para entrada)
+                       ( 6, True,  True , config_global.LED  , 'Rojo'                    ), # el tercero, la activación si es de salida (True si es activo a alto nivel o False si es a bajo nivel) o el estado si es de entrada (True si está bajado y False subido)
+                       ( 5, True,  True , config_global.LED  , 'Alarma'                  ), # y el cuarto, una muy breve descripción de su función
                       ]
 
-    PAUSA           = 10                                                            # PAUSA contiene el tiempo que el bucle estará parado
+    PAUSA           = 10                                                                    # PAUSA contiene el tiempo que el bucle estará parado
 
-    senyales        = {'SIGTERM': 'sig_cerrar',                                     # senyales (señales) contiene el tipo de señal y la función a la que ésta se asociará
+    senyales        = {'SIGTERM': 'sig_cerrar',                                             # senyales (señales) contiene el tipo de señal y la función a la que ésta se asociará
                        'SIGUSR1': 'sig_test',
                        'SIGUSR2': 'sig_apagado',
                       }
@@ -38,7 +38,7 @@ class config_global:                                                            
 
 
 class dht11_config(config_global):
-    GPIOS           = [(25, False, False, self.SONDA, 'Sonda DHT11 de pruebas'  ),
+    GPIOS           = [(25, False, False, config_global.SONDA, 'Sonda DHT11 de pruebas'  ),
                       ]
 
     LIMITE          = 10
@@ -47,22 +47,22 @@ class dht11_config(config_global):
 
 
 class domotica_cliente_config(config_global):
-    puerto          = 4710                                                          # El puerto 4710 ha sido escogido arbitrariamente por estar libre, según la IANA:
-                                                                                    # https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?&page=85
+    puerto          = 4710                                                                  # El puerto 4710 ha sido escogido arbitrariamente por estar libre, según la IANA:
+                                                                                            # https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?&page=85
 
 
 class domotica_servidor_config(domotica_cliente_config):
-    GPIOS           = [(22, False, False, self.BOTON, 'Botón reinicio router'   ),  # En este caso, los puertos GPIO serán dados por pares:
-                       ( 4, True,  False, self.RELE , 'Relé reinicio router'    ),  # Las entradas impares orresponderán a los relés que se gestionarán
+    GPIOS           = [(22, False, False, config_global.BOTON, 'Botón reinicio router'   ), # En este caso, los puertos GPIO serán dados por pares:
+                       ( 4, True,  False, config_global.RELE , 'Relé reinicio router'    ), # Las entradas impares orresponderán a los relés que se gestionarán
 
-                       (24, False, False, self.BOTON, 'Botón reinicio switch'   ),  # Las pares, a los pulsadores o equivalentes que irán asociados a dichos relés, para su conmutación
-                       (23, True,  False, self.RELE , 'Relé reinicio switch'    ),
+                       (24, False, False, config_global.BOTON, 'Botón reinicio switch'   ), # Las pares, a los pulsadores o equivalentes que irán asociados a dichos relés, para su conmutación
+                       (23, True,  False, config_global.RELE , 'Relé reinicio switch'    ),
 
-                       (17, False, False, self.BOTON, 'Botón reinicio cámara'   ),
-                       (27, True,  False, self.RELE , 'Relé reinicio cámara'    ),
+                       (17, False, False, config_global.BOTON, 'Botón reinicio cámara'   ),
+                       (27, True,  False, config_global.RELE , 'Relé reinicio cámara'    ),
 
-                       (13, False, False, self.SONDA, 'Indicador electricidad'  ),
-                       (16, True,  False, self.RELE , 'Relé activación router'  ),
+                       (13, False, False, config_global.SONDA, 'Indicador electricidad'  ),
+                       (16, True,  False, config_global.RELE , 'Relé activación router'  ),
                       ]
 
     LLAMADAS        = [(None,                    False, False),
@@ -80,7 +80,7 @@ class domotica_servidor_config(domotica_cliente_config):
 
 
 class internet_config(config_global):
-    HOSTS           = ['ra.routers.veltys.es',                                      # HOSTS contiene los servidores a los cuales se les hará ping para comprobar si hay internet
+    HOSTS           = ['ra.routers.veltys.es',                                              # HOSTS contiene los servidores a los cuales se les hará ping para comprobar si hay internet
                        'plus.servidores.veltys.es',
                        'veltys.es',
                        'google.es',
@@ -110,21 +110,21 @@ class internet_config(config_global):
 
 
 ''' class temperatura_config(config_global):
-    COLORES         = [(0.0, 0.0, 1.0, 0.0),                                        # COLORES contiene una matriz de 4 x 4 que, por columnas, representa cada led y, por filas, la etapa de temperatura
+    COLORES         = [(0.0, 0.0, 1.0, 0.0),                                                # COLORES contiene una matriz de 4 x 4 que, por columnas, representa cada led y, por filas, la etapa de temperatura
                        (0.0, 1.0, 0.0, 0.0),
                        (1.0, 0.6, 0.0, 0.0),
                        (1.0, 0.0, 0.0, 1.0),
                       ]
 
-    FRECUENCIA      = 60                                                            # FRECUENCIA contiene la frecuencia (en herzios) de refresco de los leds
+    FRECUENCIA      = 60                                                                    # FRECUENCIA contiene la frecuencia (en herzios) de refresco de los leds
 
-    GPIOS           = [(16, True,  True , self.LED  , 'Frío'                    ),
-                       (20, True,  True , self.LED  , 'Intermedio'              ),
-                       (21, True,  True , self.LED  , 'Caliente'                ),
-                       (12, True,  True , self.LED  , 'Alarma'                  ),
+    GPIOS           = [(16, True,  True , config_global.LED  , 'Frío'                    ),
+                       (20, True,  True , config_global.LED  , 'Intermedio'              ),
+                       (21, True,  True , config_global.LED  , 'Caliente'                ),
+                       (12, True,  True , config_global.LED  , 'Alarma'                  ),
                       ]
 
-    TEMPERATURAS    = [40, 45, 50]                                                  # TEMPERATURAS contiene las temperaturas de activación de cada etapa
+    TEMPERATURAS    = [40, 45, 50]                                                          # TEMPERATURAS contiene las temperaturas de activación de cada etapa
 
     PAUSA           = 60
 
